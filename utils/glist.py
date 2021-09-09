99145af215a0d5ec22f8ac0d4d68a859c5e40600
liguopeng <liguopeng@liguopeng.net>
2021-09-09 20:18:52 +0800

    update requirements

--- utils/glist.py
+++ utils/glist.py
@@ -5,3 +5,11 @@
 
 def split(list_obj, count):
     return list_obj[:count], list_obj[count:]
+
+
+def to_string(x):
+    return str(x) if type(x) != unicode else x.encode("utf-8")
+
+
+def to_string_list(items):
+    return [to_string(item) for item in items]
